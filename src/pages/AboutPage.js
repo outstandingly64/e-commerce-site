948ebb35423d5e9ecfd1e2e1f736fe1ec4b3f762
Ivan Tello <ivantello9922@gmail.com>
2022-12-13 11:36:15 -0500

    About Page, Checkout Page setup

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,45 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title={`about`}/>
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut
+            ullamcorper libero. Suspendisse rhoncus, purus vitae molestie
+            cursus, urna ante luctus enim, ac euismod ex nunc vel velit. Donec
+            pulvinar lobortis facilisis. Phasellus luctus mauris vel bibendum
+            posuere. Integer venenatis nibh sodales diam lacinia, at semper
+            ligula egestas. Donec iaculis consectetur quam. Phasellus vel
+            pharetra diam. Nulla aliquet vulputate odio ac finibus. Etiam in
+            arcu nec nibh tempus faucibus. Nulla malesuada magna at porttitor
+            hendrerit. Donec id magna vulputate, sodales purus nec, rhoncus
+            magna. Etiam sit amet rhoncus lacus. Integer tempor tristique velit
+            at convallis. Proin ullamcorper, nibh vitae suscipit finibus, mi
+            dolor congue turpis, in vulputate felis odio id libero. Sed blandit
+            diam libero, id auctor ligula tempor sit amet. Cras a lacus eget
+            erat dapibus scelerisque. Aliquam fermentum urna sit amet facilisis
+            tristique. Curabitur quis placerat metus. Proin laoreet purus
+            sagittis luctus ullamcorper. Pellentesque suscipit vitae tellus non
+            consequat. Praesent imperdiet nec leo sed facilisis. Phasellus diam
+            nisl, auctor id odio eget, blandit commodo lacus. Proin quis
+            fermentum lacus.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +67,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
+import React from "react";
+import styled from "styled-components";
+import { PageHero, StripeCheckout } from "../components";
 // extra imports
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
-}
-const Wrapper = styled.div``
-export default CheckoutPage
+  return (
+    <main>
+      <PageHero title={`checkout`} />
+      <Wrapper className="page">
+        <h1>
+          CHECKOUT: Ut lobortis mollis nibh, nec accumsan est feugiat sed. Aliquam pretium
+          mi nulla, laoreet feugiat lectus finibus eget. Morbi tempus libero et
+          diam aliquam, ut fringilla dui aliquam. Fusce dictum ante eros, vel
+          bibendum mauris sagittis in.{" "}
+        </h1>
+      </Wrapper>
+    </main>
+  );
+};
+const Wrapper = styled.div``;
+export default CheckoutPage;
