4a24bfc6980fb9e300fe4b29232302147439efdc
Ivan Tello <ivantello9922@gmail.com>
2022-12-23 16:47:17 -0500

    text search filter for product functionaility

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -62,8 +62,19 @@ const filter_reducer = (state, action) => {
       return {...state, filters: {...state.filters, [name]: value}}
   }
   if(action.type === FILTER_PRODUCTS) {
-    //console.log('filtering products');
-    return {...state}
+    // always start with a fresh copy of all the products
+    const { all_products } = state;
+    const { text, category, company, color, price, shipping} = state.filters;
+
+    let tempProducts = [...all_products];
+    //filtering
+    if(text){
+      tempProducts = tempProducts.filter((product)=>{
+        return product.name.toLowerCase().startsWith(text.toLowerCase());
+      });
+    }
+
+    return {...state, filtered_products: tempProducts}
 }
 if(action.type === CLEAR_FILTERS){
   return {...state, filters:{
