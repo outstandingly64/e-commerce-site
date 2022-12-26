9027ced59c6330dac436a50af77709dfe34f8562
Ivan Tello <ivantello9922@gmail.com>
2022-12-25 23:21:05 -0500

    cart content setup

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
-import CartColumns from './CartColumns'
-import CartItem from './CartItem'
-import CartTotals from './CartTotals'
+import React from "react";
+import styled from "styled-components";
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
+import CartColumns from "./CartColumns";
+import CartItem from "./CartItem";
+import CartTotals from "./CartTotals";
 
 const CartContent = () => {
-  return <h4>cart content </h4>
-}
+  const { cart, clearCart } = useCartContext();
+
+  const cartItems = cart.map((item) => {
+    return <CartItem key={item.id} {...item}/>
+  });
+
+  return <Wrapper className="section section-center">
+    <CartColumns/>
+    {cartItems}
+    <hr/>
+    <div className="link-container">
+      <Link to="/products" className="link-btn">
+        Continue Shopping
+      </Link>
+      <button className="link-btn clear-btn" type="button" onClick={clearCart}>
+        clear cart
+      </button>
+    </div>
+    <CartTotals/>
+  </Wrapper>;
+};
 const Wrapper = styled.section`
   .link-container {
     display: flex;
@@ -30,5 +49,5 @@ const Wrapper = styled.section`
   .clear-btn {
     background: var(--clr-black);
   }
-`
-export default CartContent
+`;
+export default CartContent;
