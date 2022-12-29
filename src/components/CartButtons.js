63e85403257d32b03cec0426dfa786414b7c16c9
Ivan Tello <ivantello9922@gmail.com>
2022-12-29 18:01:32 -0500

    auth0 button functionality

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -27,16 +27,19 @@ const CartButtons = () => {
           <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn" onClick={loginWithRedirect}>
-        Log In <FaUserPlus />
-      </button>
-      <button
-        type="button"
-        className="auth-btn"
-        onClick={() => logout({ returnTo: window.location.origin })}
-      >
-        Logout <FaUserMinus />
-      </button>
+      {myUser ? (
+        <button
+          type="button"
+          className="auth-btn"
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type="button" className="auth-btn" onClick={loginWithRedirect}>
+          Log In <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
   );
 };
