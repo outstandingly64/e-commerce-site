16d1315a22161dce1e2ed5ccdde11e8d5d6e9ffb
Ivan Tello <ivantello9922@gmail.com>
2022-12-29 14:53:00 -0500

    Logged In User Context setup

--- src/index.js
+++ src/index.js
@@ -17,12 +17,14 @@ root.render(
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
   </Auth0Provider>
 );
